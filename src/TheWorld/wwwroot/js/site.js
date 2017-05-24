3a8b1cce7ace173ac412c74e0af1582906cec603
Tom <Tom@TOM-DESKTOP>
2017-05-24 12:58:40 -0400

    Add project files.

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿// site.js
+(function () {
+
+    
+
+    //var ele = $("#username");
+    //ele.text("Tom George");
+
+    //var main = $("main");
+    //main.on("mouseenter", function () {
+    //    main.style.backgroundColor = "#888";
+    //});
+
+    //main.on("mouseleave", function () {
+    //    main.style.backgroundColor = "";
+    //});
+
+    //var menuitems = $("ul.menu li a");
+    //menuitems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+})();
+
